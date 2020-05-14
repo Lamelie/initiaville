373027cda87d81f3d05e6382cc8b92e5bece3ca0
Lamelie <ameliederoche@hotmail.com>
2020-05-14 09:39:17 +0200

    création du CSS et du HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
 
+import 'bootstrap';
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
